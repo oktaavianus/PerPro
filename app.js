fc114ff5f108baab8b9d061041513ab1e8eb2e54
susilaattari <susilatropikanew@gmail.com>
2022-02-24 18:05:58 +0700

    home

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 const router = require('./router')
 
-app.set ('view engine', 'ejs')
+app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended : false}))
 
 app.use(router)
--- models/product.js
+++ models/product.js
@@ -56,3 +56,5 @@ module.exports = (sequelize, DataTypes) => {
   });
   return Product;
 };
+
+
