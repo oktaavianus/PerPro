439575c339e62417fcfb2a1f3e090ed69e68a9d5
oktaavianus <oktaavianus.kurniadi@gmail.com>
2022-02-24 12:51:07 +0700

    base file update

---
+++ models/product.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Product.belongsTo(models.Category, { foreignKey: "CategoryId" })
+      Product.hasMany(models.Cart, {foreignKey: "ProductId" })
+    }
+  }
+  Product.init({
+    name: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    stock: DataTypes.INTEGER,
+    imageUrl: DataTypes.STRING,
+    description: DataTypes.STRING,
+    CategoryId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Product',
+  });
+  return Product;
+};
